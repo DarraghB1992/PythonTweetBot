727655376feacdedae471bcde2defe7960dfe758
DarraghB1992 <darragh1992@gmail.com>
2016-02-05 15:44:16 +0000

    Intial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Python Tweet Bot
+==========
+
+A python twitter-bot which tweets from a .txt file at a user set time
+
+**How To Use **
+--------------
+* Add your Twitter API keys in the appropriate section
+* Set the delay in seconds inside the brackets in time.sleep()
+* Be careful not too tweet too often or twitter will lock you out
+* The bot tweets line by line so take this into account when you edit your tweets
+* Run by navigating to the folder and running the : **python tweet_bot.py tweet_bot.txt** command
+* Sit back and watch the tweets go :D
---
+++ tweet_bot.py
@@ -0,0 +1,24 @@
+# Made By Darragh Browne
+# using the guide at http://www.dototot.com/how-to-write-a-twitter-bot-with-python-and-tweepy/
+import tweepy, time, sys
+from tweepy import OAuthHandler
+
+
+CONSUMER_KEY = 'YourKeyHere'
+CONSUMER_SECRET = 'YourKeyHere'
+OAUTH_TOKEN = 'YourKeyHere-vMHsKwAm3NXlgi3TCUQbANKtX5z53aebTQb7KVPC'
+OAUTH_TOKEN_SECRET = 'YourKeyHere'
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+argfile = str(sys.argv[1])
+
+filename = open(argfile, 'r')
+f = filename.readlines()
+filename.close()
+# time.sleep determines the delay between tweets in seconds , be aware if you tweet too often twitter will kick you out
+for line in f:
+    api.update_status(status=line)
+    time.sleep(240)ls
